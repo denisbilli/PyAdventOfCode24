e19db1c340ed3adf4bf62c462e233ca452d1135e
Denis Billi <dbilli@corp.scmgroup.com>
2024-12-18 18:44:23 +0100

    added skeleton for exercise 14

---
+++ exercise14.py
@@ -0,0 +1,23 @@
+
+from file_utils import *
+from matrix_utils import *
+from string_utils import *
+
+
+print("-----------------------------\nPART 1")
+
+file_path = f"./inputs/14/test.txt"
+file_content = read_file_to_string(file_path)
+
+
+print(f"\nSOLUTION --------------------\n")
+
+
+print(f"solution: \n")
+
+print("-----------------------------\nPART 2")
+
+
+print(f"\nSOLUTION --------------------\n")
+
+print(f"solution: \n")
